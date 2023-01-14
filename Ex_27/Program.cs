b3605eadbbe4fd803f9fba4197a71ce5309f7daa
Kazbek <bender.rodrigiz2014@gmail.com>
2023-01-15 00:05:04 +0300

    Домашнее задание

---
+++ Ex_27/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Задача 27: 
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+ */
+
+/* string a = "321";
+int s = a.Length;
+Console.WriteLine(s); */
+Console.Clear();
+
+int num, leng, sum, i, remains; string num_str; // обьявление переменных и их тип данных
+
+
+while (true)
+{
+    Console.WriteLine("Введите число и получите сумму цифр в числе: ");
+    num_str = Console.ReadLine(); // ввод данных
+    if (!int.TryParse(num_str, out num)) // првоерка на ввод числа
+    {
+        Console.WriteLine("Вы ввели не число!");
+    }
+    else break;
+}
+
+leng = num_str.Length; // получение колличества символов для использовния в цикле 
+num = int.Parse(num_str); // преобразование  string в integer
+sum = 0;
+remains = 0;
+
+if (num < 0) leng--; // проверка на положительное число 
+
+for (i = 0; i < leng; i++)
+{
+    remains = num % 10;
+    sum += remains;
+    num /= 10;
+}
+
+
+Console.WriteLine("Сумма цифр в числе {0} равна = {1}", num_str, sum);
+
+
+
+
+
---
+++ Ex_29/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 29: 
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. */
+Console.Clear();
+
+int  size, i, num_ar; Random r = new Random(); // обьявление переменных 
+
+Console.WriteLine("Введите размер массива:");
+size = int.Parse(Console.ReadLine()); // задаем размер массива 
+int [] array = new int [size]; // создаем массив размер которого определяет "size"
+
+// здаем значения в массив сами
+for (i = 0; i < size; i++)
+{
+    Console.WriteLine("Введите числа которое хотите добавить в массив:");
+    num_ar = int.Parse(Console.ReadLine());
+    array [i] = num_ar; // присваеваем новые значения в массив
+}
+
+// задаем значения в массив рандомно
+/* for (i = 0; i < size; i++)
+{
+    num_ar = r.Next(0, 100000000);
+    array [i] = num_ar; // присваеваем новые значения в массив
+} */
+
+var str = string.Join(" ", array);
+Console.WriteLine($"Числа которые добавили в массив {str}");
+
+
+
+/* foreach (int array_new in array )
+{
+    Console.WriteLine($"Числа которые добавили в массив {array_new} ");
+} */
